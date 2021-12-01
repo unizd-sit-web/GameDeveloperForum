187d78d565d1a5cea000e6ba966d692300f2df18
MRumstajn <mrumstajn@student.unizd.hr>
2021-12-01 15:36:16 +0100

    added function for creating post cards

--- util.js
+++ util.js
@@ -14,3 +14,23 @@ export function createThreadCard(title, redirUrl){
     threadDiv.append(`<h2><a href="${redirUrl}">${title}</a></h2>`)
     return threadDiv
 }
+
+/**
+ * 
+ * @param {string} content 
+ * @param {string} author 
+ * @param {string} createDate 
+ * @param {string} editDate 
+ * @returns Card as a DOM element
+ */
+export function createPostCard(content, author, createDate, editDate){
+    let card = $("<div>").addClass("card")
+    let cardBody = $("<div>").addClass("card-body")
+    let cardTitle = $("<h5>").addClass("card-title").text(author)
+    let cardText = $("<p>").addClass("card-text").text(content)
+    cardBody.append(cardTitle)
+    cardBody.append(cardText)
+    card.append(cardBody)
+
+    return card
+}
